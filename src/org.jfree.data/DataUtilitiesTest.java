ca3d3219dba60b01ccca354bdfd67469f4dbe6e2
Clean <cjdrewes@gmail.com>
2023-03-17 00:41:42 -0600

    Added Reports and finished increasing Mutation catch rates
    HUNDO PERCENTO BAYBEEEEEOOOOOOOO

---
+++ src/org.jfree.data/DataUtilitiesTest.java
@@ -0,0 +1,297 @@
+package org.jfree.data;
+
+import static org.junit.Assert.*;
+
+import org.jfree.data.DataUtilities;
+import org.jfree.data.DefaultKeyedValues;
+import org.jfree.data.KeyedValues;
+import org.jfree.data.Values2D;
+import org.jmock.Expectations;
+import org.jmock.Mockery;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+public class DataUtilitiesTest extends DataUtilities {
+    static Mockery mockingContext;
+    static Values2D values;
+
+    @BeforeClass
+    public static void setUpBeforeClass() {
+        mockingContext = new Mockery();
+        values = mockingContext.mock(Values2D.class);
+    }
+
+    @Test
+    public void equalForNULL() {
+        boolean result = DataUtilities.equal(null, null);
+        assertEquals(true, result);
+    }
+
+    @Test
+    public void equalOneNull() {
+        double [][] a = {{0},{1}};
+        boolean result = DataUtilities.equal(null, a);
+        assertEquals(false, result);
+    }
+
+    @Test
+    public void equalOtherOneNull() {
+        double [][] a = {{0},{1}};
+        boolean result = DataUtilities.equal(a, null);
+        assertEquals(false, result);
+    }
+
+    @Test
+    public void equalDiffLength() {
+        double [][] a = {{0},{1}};
+        double [][] b = {{0},{1}, {2}};
+        boolean result = DataUtilities.equal(a, b);
+        assertEquals(false, result);
+    }
+
+    @Test
+    public void equalSameArrays() {
+        double [][] a = {{0},{1},{2}};
+        double [][] b = {{0},{1},{2}};
+        boolean result = DataUtilities.equal(a, b);
+        assertEquals(true, result);
+    }
+
+    @Test
+    public void equalDiffArrays() {
+        double [][] a = {{0},{1},{2}};
+        double [][] b = {{1},{2},{3}};
+        boolean result = DataUtilities.equal(a, b);
+        assertEquals(false, result);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testCloneNULL(){
+        DataUtilities.clone(null);
+    }
+
+    @Test
+    public void testCloneWithArray() {
+        double[][] a = {{1}, {2}, {3}};
+        double [][] result = DataUtilities.clone(a);
+        assertEquals(Arrays.deepToString(a), Arrays.deepToString(result));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testCalculateColumnTotalNull(){
+        DataUtilities.calculateColumnTotal(null, 8);
+    }
+
+    @Test
+    public void calculateColumnTotalForFiveValues() {
+        // setup
+        mockingContext.checking(new Expectations() {
+            {
+                one(values).getRowCount();
+                will(returnValue(5));
+                one(values).getValue(0, 0);
+                will(returnValue(7.5));
+                one(values).getValue(1, 0);
+                will(returnValue(2.5));
+                one(values).getValue(2, 0);
+                will(returnValue(0));
+                one(values).getValue(3, 0);
+                will(returnValue(-10.4));
+                one(values).getValue(4, 0);
+                will(returnValue(5.4));
+            }
+        });
+        // test
+        double result = DataUtilities.calculateColumnTotal(values, 0);
+        // verify
+        assertEquals(5.0, result, .000000001d);
+    }
+
+    @Test
+    public void calculateColumnTotalForTwoValues() {
+        // setup
+        mockingContext.checking(new Expectations() {
+            {
+                one(values).getRowCount();
+                will(returnValue(2));
+                one(values).getValue(0, 1);
+                will(returnValue(7.5));
+                one(values).getValue(1, 1);
+                will(returnValue(-2.5));
+            }
+        });
+        // test
+        double result = DataUtilities.calculateColumnTotal(values, 1);
+        // verify
+        assertEquals(5.0, result, .000000001d);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testCalculateColumnTotalValidRowsNull(){
+        int[] validRows = {0,1,4};
+        DataUtilities.calculateColumnTotal(null, 8, validRows);
+    }
+
+    @Test
+    public void calculateColumnTotalValidRows() {
+        // setup
+        mockingContext.checking(new Expectations() {
+            {
+                one(values).getRowCount();
+                will(returnValue(5));
+                one(values).getValue(0, 0);
+                will(returnValue(7.5));
+                one(values).getValue(1, 0);
+                will(returnValue(2.5));
+                one(values).getValue(2, 0);
+                will(returnValue(0));
+                one(values).getValue(3, 0);
+                will(returnValue(-10.4));
+                one(values).getValue(4, 0);
+                will(returnValue(5.4));
+            }
+        });
+        int[] validRows = {0,1,4,5};
+
+        // test
+        double result = DataUtilities.calculateColumnTotal(values, 0, validRows);
+        // verify
+        assertEquals(15.4, result, .000000001d);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testCalculateRowTotalNull(){
+        DataUtilities.calculateRowTotal(null, 8);
+    }
+
+    @Test
+    public void calculateRowTotalForFiveValues() {
+        // setup
+        mockingContext.checking(new Expectations() {
+            {
+                one(values).getColumnCount();
+                will(returnValue(5));
+                one(values).getValue(0, 0);
+                will(returnValue(7.5));
+                one(values).getValue(0, 1);
+                will(returnValue(2.5));
+                one(values).getValue(0, 2);
+                will(returnValue(0));
+                one(values).getValue(0, 3);
+                will(returnValue(-10.4));
+                one(values).getValue(0, 4);
+                will(returnValue(5.4));
+            }
+        });
+        // test
+        double result = DataUtilities.calculateRowTotal(values, 0);
+        // verify
+        assertEquals(5.0, result, .000000001d);
+    }
+
+    @Test
+    public void calculateRowTotalForNegativeTwoValues() {
+        // setup
+        mockingContext.checking(new Expectations() {
+            {
+                one(values).getColumnCount();
+                will(returnValue(2));
+                one(values).getValue(1, 0);
+                will(returnValue(-7.5));
+                one(values).getValue(1, 1);
+                will(returnValue(2.5));
+            }
+        });
+        // test
+        double result = DataUtilities.calculateRowTotal(values, 1);
+        // verify
+        assertEquals(-5.0, result, .000000001d);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testCalculateRowTotalValidRowsNull(){
+        int[] validRows = {0,1,4};
+        DataUtilities.calculateRowTotal(null, 8, validRows);
+    }
+
+    @Test
+    public void calculateRowTotalValidRows() {
+        // setup
+        mockingContext.checking(new Expectations() {
+            {
+                one(values).getColumnCount();
+                will(returnValue(5));
+                one(values).getValue(0, 0);
+                will(returnValue(7.5));
+                one(values).getValue(0, 1);
+                will(returnValue(2.5));
+                one(values).getValue(0, 2);
+                will(returnValue(0));
+                one(values).getValue(0, 3);
+                will(returnValue(-10.4));
+                one(values).getValue(0, 4);
+                will(returnValue(5.4));
+            }
+        });
+        int[] validRows = {0,1,4,5};
+        // test
+        double result = DataUtilities.calculateRowTotal(values, 0, validRows);
+        // verify
+        assertEquals(15.4, result, .000000001d);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void createNumberArrayTestNull() {
+        DataUtilities.createNumberArray(null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void createNumberArray2DTestNull() {
+        DataUtilities.createNumberArray2D(null);
+    }
+
+    @Test
+    public void createNumberArrayTestWithValues() {
+        // setup
+        double[] doubleArray = {1.2, 8.65, -10.9, 5.4, -4.35};
+        //test
+        Number[] numberArray = DataUtilities.createNumberArray(doubleArray);
+        //verify
+        assertEquals(Arrays.toString(doubleArray), Arrays.toString(numberArray));
+    }
+
+    @Test
+    public void createNumberArray2DTestWithValues() {
+        // setup
+        double[][] doubleArray = {
+                {1.2, 8.65, -10.9, 5.4, -4.35},
+                {1.2, 8.65, -10.9, 5.4, -4.35},
+                {1.2, 8.65, -10.9, 5.4, -4.35},
+                {1.2, 8.65, -10.9, 5.4, -4.35},
+        };
+        //test
+        Number[][] numberArray = DataUtilities.createNumberArray2D(doubleArray);
+        // Verify
+        assertEquals(Arrays.deepToString(doubleArray), Arrays.deepToString(numberArray));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void getCumulativePercentagesTestNull() {
+        DataUtilities.getCumulativePercentages(null);
+    }
+
+    @Test
+    public void getCumulativePercentagesTest() {
+        //setup
+        DefaultKeyedValues test = new DefaultKeyedValues();
+        test.addValue((Comparable<?>) 0, 5);
+        test.addValue((Comparable<?>) 1, 9);
+        test.addValue((Comparable<?>) 2, 2);
+        //test
+        KeyedValues result = DataUtilities.getCumulativePercentages(test);
+        //verify
+        assertEquals(0.875, result.getValue(1).doubleValue(), .000000001d);
+    }
+}
---
+++ src/org.jfree.data/RangeTest.java
@@ -0,0 +1,530 @@
+package org.jfree.data;
+
+import static org.junit.Assert.*;
+
+import org.junit.*;
+
+public class RangeTest {
+    private static Range exampleRange, largeRange;
+
+    @BeforeClass
+    public static void setUpBeforeClass() {
+        exampleRange = new Range(-1, 1);
+        largeRange = new Range(-99995.875, 139995.875);
+
+    }
+    
+    
+    // CONSTRAIN
+    @Test
+    public void getConstrainInRangeTest() {   	
+        assertEquals(0, exampleRange.constrain(0), .000000001d);
+    }
+    
+    @Test
+    public void getConstrainOutBiggerTest() {   	
+        assertEquals(1, exampleRange.constrain(2), .000000001d);
+    }
+    
+    @Test
+    public void getConstrainOutSmallerTest() {   	
+        assertEquals(-1, exampleRange.constrain(-2), .000000001d);
+    }
+    
+    @Test
+    public void getConstrainBoundaryTest() {   	
+    	assertEquals(1, exampleRange.constrain(1), .000000001d);
+    }
+    
+    @Test
+    public void getConstrainNegBoundaryTest() {   	
+    	assertEquals(-1, exampleRange.constrain(-1), .000000001d);
+    }
+    
+    @Test
+    public void getConstrainbetween0andUpperTest() {   	
+    	assertEquals(0.5, exampleRange.constrain(0.5), .000000001d);
+    }
+    
+    @Test
+    public void getConstrainbetween0andlowerTest() {   	
+    	assertEquals(-0.5, exampleRange.constrain(-0.5), .000000001d);
+    }
+
+    
+    // COMBINE
+    @Test
+    public void getCombineTest() {   
+    	Range combined = new Range(-99995.875, 139995.875);
+    	assertEquals(combined, Range.combine(exampleRange, largeRange));
+    }
+    
+    @Test
+    public void getCombineRange1NullTest() {   
+    	assertEquals(largeRange, Range.combine(null, largeRange));
+    }
+    
+    @Test
+    public void getCombineRange2NullTest() {   
+    	assertEquals(exampleRange, Range.combine(exampleRange, null));
+    }
+    
+    @Test
+    public void getCombineRangeZeroTest() { 
+    	Range combined = new Range(0,0);
+    	assertEquals(combined, Range.combine(combined,combined));
+    }
+    
+    //EXPAND TO INCLUDE
+    @Test
+    public void getExpandToIncludeNullTest() {   
+    	Range expected = new Range(2, 2);
+    	Range result = Range.expandToInclude(null,2);
+    	assertEquals(expected, result);
+    }
+    
+    @Test
+    public void getExpandToIncludeUpperTest() {   
+    	Range expected = new Range(-1, 2);
+    	Range result = Range.expandToInclude(exampleRange,2);
+    	assertEquals(expected, result);
+    }
+    
+    @Test
+    public void getExpandToIncludeLowerTest() {   
+    	Range expected = new Range(-2, 1);
+    	Range result =  Range.expandToInclude(exampleRange,-2);
+    	assertEquals(expected,result);
+    }
+
+    @Test
+    public void getExpandToIncludeTest() {   
+    	Range expected = new Range(-1, 1);
+    	Range result = Range.expandToInclude(exampleRange,1);
+    	assertEquals(expected, result);
+    }
+    @Test
+    public void getExpandToIncludeTestValequaltolower() {   
+    	Range expected = new Range(-1, 1);
+    	Range result = Range.expandToInclude(exampleRange,-1);
+    	assertEquals(expected,result);
+    }
+    
+    @Test
+    public void getExpandToIncludeTestValequaltozero() {   
+    	Range expected = new Range(-1, 1);
+    	Range result = Range.expandToInclude(exampleRange,0);
+    	assertEquals(expected,result);
+    }
+
+    // expand
+    @Test
+    public void getExpandTest() {
+        Range expected = new Range(3, 5);
+    	assertEquals(expected, Range.expand(exampleRange,-2,2));
+    }
+    @Test
+    public void getExpandTestEqual(){
+        Range expected = new Range(-7,-7);
+    	assertEquals(expected, Range.expand(exampleRange,3,-4));
+    }
+    @Test
+    public void getExpandTestlowerbigger(){
+        Range expected = new Range(0,0);
+    	assertEquals(expected, Range.expand(exampleRange,-3,-3));
+    }
+    @Test
+    public void getExpandTestlowersmaller(){
+        Range expected = new Range(-7,7);
+    	assertEquals(expected, Range.expand(exampleRange,3,3));
+    }
+
+    
+    //SHIFT
+    @Test
+    public void getShiftTest() {   
+    	Range expected = new Range(0, 3);
+    	Range result = Range.shift(exampleRange,2);
+    	assertEquals(expected, result);
+    }
+    @Test
+    public void getShiftTestZero() {   
+    	Range expected = new Range(-1, 1);
+    	Range result = Range.shift(exampleRange,0);
+    	assertEquals(expected, result);
+    }
+    
+    @Test
+    public void getShiftTrueTest() {   
+    	Range expected = new Range(1, 3);
+    	Range result = Range.shift(exampleRange,2, true);
+    	assertEquals(expected, result);
+    }
+    
+    @Test
+    public void getShiftFalseTest() {   
+    	Range expected = new Range(0, 3);
+    	Range result =  Range.shift(exampleRange,2, false);
+    	assertEquals(expected,result);
+    }
+    
+    @Test
+    public void getShiftNegTrueTest() {   
+    	Range expected = new Range(-3, -1);
+    	Range result = Range.shift(exampleRange,-2, true);
+    	assertEquals(expected, result);
+    }
+    
+    @Test
+    public void getShiftBetweenZeroAndOne() {   
+    	Range expected = new Range(-0.5, 0.5);
+    	Range ran = new Range (-0.5,0.5);
+    	Range result = Range.shift(ran,0, false);
+    	assertEquals(expected, result);
+    }
+    
+    @Test
+    public void getShiftBetweenZeroAndOnesecond() {   
+    	Range expected = new Range(0.0,0.0);
+    	Range ran = new Range (0.0,0.0);
+    	Range result = Range.shift(ran,0.0, false);
+    	assertEquals(expected, result);
+    }
+    
+    @Test
+    public void getShiftBetweenZeroAndOnethird() {   
+    	Range expected = new Range(1.1,1.15);
+    	Range ran = new Range (0.1,0.15);
+    	Range result = Range.shift(ran,1, false);
+    	assertEquals(expected, result);
+    }
+    
+    
+    
+    //CENTRAL
+    @Test
+    public void getCentralValueTestZero() {
+        assertEquals(0, exampleRange.getCentralValue(), .000000001d);
+    }
+
+    @Test
+    public void getCentralValueTestLargeRange() {
+        assertEquals(20000, largeRange.getCentralValue(), .000000001d);
+    }
+
+    //length
+    @Test
+    public void getLengthTestTwo() {
+        assertEquals(2, exampleRange.getLength(), .000000001d);
+    }
+
+    @Test
+    public void getLengthTestLargeRange() {
+        assertEquals(239991.75, largeRange.getLength(), .000000001d);
+    }
+
+    //lowerBound
+    @Test
+    public void getLowerBoundTestNegOne() {
+        assertEquals(-1, exampleRange.getLowerBound(), .000000001d);
+    }
+
+    @Test
+    public void getLowerBoundTestLargeRange() {
+        assertEquals(-99995.875, largeRange.getLowerBound(), .000000001d);
+    }
+
+    @Test
+    public void getIntersects() {
+    	Range test = new Range(1, 3);
+        assertTrue(largeRange.intersects(test));
+    }
+
+    @Test
+    public void getIntersectsFalse() {
+        Range test = new Range(6, 25);
+        assertFalse(exampleRange.intersects(test));
+    }
+    //upperBound
+    @Test
+    public void getUpperBoundTestOne() {
+        assertEquals(1, exampleRange.getUpperBound(), .000000001d);
+    }
+
+    @Test
+    public void getUpperBoundTestLargeRange() {
+        assertEquals(139995.875, largeRange.getUpperBound(), .000000001d);
+    }
+
+    //contains
+    @Test
+    public void containsTestOneInRange() {
+        boolean actual = exampleRange.contains(1);
+        assertTrue(actual);
+    }
+
+    @Test
+    public void containsTestNegOneInRange() {
+        boolean actual = exampleRange.contains(-1);
+        assertTrue(actual);
+    }
+
+    @Test
+    public void containsTestZeroInRange() {
+        boolean actual = largeRange.contains(0);
+        assertTrue(actual);
+    }
+
+    @Test
+    public void containsTestTwoOutOfRange() {
+        boolean actual = exampleRange.contains(2);
+        assertFalse(actual);
+    }
+
+    @Test
+    public void containsTestLargeNegOutOfRange() {
+        boolean actual = largeRange.contains(-99999);
+        assertFalse(actual);
+    }
+
+    @Test
+    public void containsTestLargePosOutOfRange() {
+        boolean actual = largeRange.contains(140000);
+        assertFalse(actual);
+    }
+    
+    @Test
+    public void containsBoundaryTest() {
+    	Range newRange = new Range(1,1);
+        boolean actual = newRange.contains(0);
+        assertFalse(actual);
+    }
+
+    //intersect
+    @Test
+    public void intersectsTestInRange() {
+        boolean actual = exampleRange.intersects(-1, 2);
+        assertTrue(actual);
+    }
+    
+    @Test
+    public void intersectsTestInRangelessb0() {
+        boolean actual = exampleRange.intersects(-2, 2);
+        assertTrue(actual);
+    }
+    
+    @Test
+    public void intersectsTestInRangebiggerb0() {
+        boolean actual = exampleRange.intersects(0, 2);
+        assertTrue(actual);
+    }
+    @Test
+    public void intersectsTestInlessRange0() {
+        boolean actual = exampleRange.intersects(0, -1);
+        assertFalse(actual);
+    }
+
+    @Test
+    public void intersectsTestRange2() {
+        boolean actual = exampleRange.intersects(1, 2);
+        assertFalse(actual);
+    }
+    @Test
+    public void intersectsTestRange3() {
+        boolean actual = exampleRange.intersects(3, 5);
+        assertFalse(actual);
+    }
+    
+    @Test
+    public void intersectsTestRange4() {
+        boolean actual = exampleRange.intersects(0, 0);
+        assertTrue(actual);
+    }
+
+    @Test
+    public void intersectsTestOutOfRangeAbove() {
+        boolean actual = largeRange.intersects(139995.876, 149995.875);
+        assertFalse(actual);
+    }
+
+    @Test
+    public void intersectsTestOutOfRangeBelow() {
+        boolean actual = largeRange.intersects(-1099995, -99995.876);
+        assertFalse(actual);
+    }
+    //COMBINE IGNORING NaN
+    @Test
+    public void CombineIgnoreNaNR1NullTest() {
+    	Range expected2 = new Range(3, 4); 
+        assertEquals(expected2, Range.combineIgnoringNaN(null, expected2));
+    }
+    @Test
+    public void CombineIgnoreNaNR1R2NullTest() {
+        assertNull(Range.combineIgnoringNaN(null, null));
+    }
+    @Test
+    public void CombineIgnoreNaNR2NullTest() {
+    	Range expected = new Range(1,2);
+        assertEquals(expected, Range.combineIgnoringNaN(expected, null));
+    }
+    @Test
+    public void CombineIgnoreNaNTest() {
+    	Range expected = new Range(1,2);
+    	Range expected2 = new Range(1,2); 
+        assertEquals(expected, Range.combineIgnoringNaN(expected, expected2));
+    }
+    
+    // SCALE
+    @Test(expected = IllegalArgumentException.class)
+    public void scaleTest() {
+    	Range expected = new Range(2,2);
+        assertEquals(expected, Range.scale(expected, -1));
+    }
+    
+    @Test
+    public void scaleTestGreaterThan0() {
+    	Range expected = new Range(2,2);
+        assertEquals(expected, Range.scale(expected, 1));
+    }
+
+    @Test
+    public void scaleTestEqual0() { 
+        Range expected = new Range(2,2); 
+        assertEquals(expected, Range.scale(expected, 0)); 
+    } 
+
+    @Test 
+    public void scaleTestBetweenZeroOne() { 
+        Range expected = new Range(2,2); 
+        assertEquals(expected, Range.scale(expected, 0.5)); 
+
+    } 
+    
+    //paramtest
+    @Test(expected = IllegalArgumentException.class)
+    public void scaleTestNullBase() {
+    	Range expected = new Range(2,2);
+        assertEquals(expected, Range.scale(null, 1));
+    }
+
+    @Test
+    public void scaleTestEqual0() { 
+    	Range ran = new Range(2,2); 
+        Range expected = new Range(0,0); 
+        assertEquals(expected, Range.scale(ran, 0)); 
+    } 
+
+    @Test 
+    public void scaleTestBetweenZeroOne() { 
+    	Range ran = new Range(2,2); 
+        Range expected = new Range(1,1); 
+        assertEquals(expected, Range.scale(ran, 0.5)); 
+
+    } 
+    
+    //EQUALS
+    @Test
+    public void equalsObjNULL() {
+        Object obj = null;
+        boolean actual = largeRange.equals(obj);
+        assertFalse(actual);
+    }
+    
+    @Test
+    public void notEqualsObj() {
+        Object obj = largeRange;
+        assertNotEquals(exampleRange, obj);
+    }
+    
+    @Test
+    public void notEqualsSmallerObj() {  	
+        Object obj = exampleRange;
+        assertNotEquals(largeRange, obj);
+    }
+    
+    @Test
+    public void equalsObj() {
+        Object obj = new Range (-99995.875, 1300);
+        boolean actual = largeRange.equals(obj);
+        assertFalse(actual);
+    }
+    
+    @Test
+    public void equalsUpperObj() {
+        Object obj = new Range (-2, 1);
+        Range ran = new Range (-3,1);
+        boolean actual = ran.equals(obj);
+        assertFalse(actual);
+    }
+    
+    @Test
+    public void equalsEqualObj() {
+        Object obj = new Range (-2, 1);
+        Range ran = new Range (-2,1);
+        boolean actual = ran.equals(obj);
+        assertTrue(actual);
+    }
+    
+    @Test
+    public void equalslowergreaterObj() {
+        Object obj = new Range (-3, 1);
+        Range ran = new Range (-2,1);
+        boolean actual = ran.equals(obj);
+        assertFalse(actual);
+    }
+    @Test
+    public void equalsuppernotequalObj() {
+        Object obj = new Range (-100, 1);
+        Range ran = new Range (-100,2);
+        boolean actual = ran.equals(obj);
+        assertFalse(actual);
+    }
+    
+    // HASH CODE
+    @Test
+    public void hashCodeTest() {
+    	assertEquals(-31457280, exampleRange.hashCode());
+    }
+
+    @Test
+    public void toStringTest() {
+        assertEquals("Range[-1.0,1.0]", exampleRange.toString());
+    }
+    
+    // isNanRange
+    @Test
+    public void isNanNoDoubleTest() {
+        assertFalse(exampleRange.isNaNRange());
+    }
+    
+    @Test
+    public void isNaNBothDoubleTest() {
+    	Range test = new Range(-1, 1);
+        assertFalse(test.isNaNRange());
+    }
+
+    @Test
+    public void isNaNBothNaNTest() {
+        Range test = new Range(Double.NaN, Double.NaN);
+        assertTrue(test.isNaNRange());
+    }
+    
+    @Test
+    public void isNanOneDoubleTest() {
+    	Range ran = new Range(-1.0, 1);
+        assertFalse(ran.isNaNRange());
+    }
+    
+    @Test
+    public void isNanOtherDoubleTest() {
+    	Range ran = new Range(-1, 1.0);
+        assertFalse(ran.isNaNRange());
+    }
+    
+    //Testing the exception thrown by constructor
+    @Test(expected = IllegalArgumentException.class)
+    public void rangeTestNullBase() {
+    	Range expected = new Range(2,-2);
+        assertEquals(expected, Range.scale(null, 1));
+    }
+}//f
